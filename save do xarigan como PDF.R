882362dcbe218aed58ad70060e5e39290f784fea
DATAUNIRIO <duttross@gmail.com>
2021-06-03 16:12:37 -0300

    apresentacao

---
+++ save do xarigan como PDF.R
@@ -0,0 +1,7 @@
+
+#remotes::install_github("jhelvy/xaringanBuilder")
+#remotes::install_github('rstudio/chromote')
+
+
+xaringanBuilder::build_pdf('C:/Users/Hp/Documents/GitHub/ap/extra-great-slides-rvest/intro/index.Rmd')
+
